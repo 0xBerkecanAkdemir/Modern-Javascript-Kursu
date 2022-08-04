ff72336c8fba472da86fbd155bd37d974ff74bb4
0xBerkecanAkdemir <berkeakdemirr@gmail.com>
2022-08-04 16:28:06 +0300

    oparatörler-

--- ders-2 operatörler/script.js
+++ ders-2 operatörler/script.js
@@ -1,51 +1,59 @@
 // OPARATÖRLER
-let val;
-const a=10;
-const b=5;
-const c=5;
-let d=3;
+// let val;
+// const a=10;
+// const b=5;
+// const c=5;
+// let d=3;
 // 1- Aritmetik Oparatörler
 
-val=a+b;
-val=a-b;
-val=a*b;
-val=a/b;
-val=a%b;
-val=d++;
-val=++d;
-val=--d;
-val=d--;
+// val=a+b;
+// val=a-b;
+// val=a*b;
+// val=a/b;
+// val=a%b;
+// val=d++;
+// val=++d;
+// val=--d;
+// val=d--;
 
 // 2-Atama Oparatörleri
 
-val=a;
-val +=a; //val =val + a;
-val -=a; //val =val - a;
-val *=a; //val =val * a;
-val /=a; //val =val / a;
-val %=a; //val =val % a;
+// val=a;
+// val +=a; //val =val + a;
+// val -=a; //val =val - a;
+// val *=a; //val =val * a;
+// val /=a; //val =val / a;
+// val %=a; //val =val % a;
 
 // 3-Karşılaştırma Oparatörleri
 
-val= a==b;
-val= b==c;
-val= a===b; // 2 ile 3 eşittir arasındaki fark 3 eşittir hem değer hemde tip kontrolü yapıyor 5 === '5' false verir 5 === 5 olsa true verir. 
-val=a!=b; //a b ye eşit değil mi?
-val=a!==b;//a b ye eşit değil mi?
-val=a > b;
-val=b < a;
-val=a >=b;
-val=5 >=5;
-val=a <=b; 
+// val= a==b;
+// val= b==c;
+// val= a===b; // 2 ile 3 eşittir arasındaki fark 3 eşittir hem değer hemde tip kontrolü yapıyor 5 === '5' false verir 5 === 5 olsa true verir. 
+// val=a!=b; //a b ye eşit değil mi?
+// val=a!==b;//a b ye eşit değil mi?
+// val=a > b;
+// val=b < a;
+// val=a >=b;
+// val=5 >=5;
+// val=a <=b; 
 
 // 4- MANTIKSAL OPARATÖRLER
 
 //&& (And ve anlamına gelir)
+// && de 1 false olsa cevap false olur
+
 //Soru a, b ve c den büyükmü bir tanesi false olursa cevap false olur ?
-val= (a>b) && (a>c); 
+// val= (a>b) && (a>c); 
 
+// || (OR - VEYA)
 
+// true - true => true
+// true - true => true
+// false - false=> false
 
+// val=(a>b) || (a>c);
 
-console.log(val);
-console.log(typeof val);
+// ! (NOT- OLUMSUZLUK)
+
+// val= !(a>b) //false gelir
