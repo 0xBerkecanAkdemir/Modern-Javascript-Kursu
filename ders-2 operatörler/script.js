2c492c88a8157b084e8457ac269c4928eaa41c38
0xBerkecanAkdemir <berkeakdemirr@gmail.com>
2022-08-04 17:22:01 +0300

    operatörler-finish

--- ders-2 operatörler/script.js
+++ ders-2 operatörler/script.js
@@ -57,3 +57,45 @@
 // ! (NOT- OLUMSUZLUK)
 
 // val= !(a>b) //false gelir
+
+//**************************
+
+//JAVASCRİPT OPARATÖR UYGULAMALARI
+
+// 1 - Can ve Ada ' nın boy ve kg bilgilerini alın.
+// 2 - Alınan bilgilere göre kilo indekslerini hesaplayınız.
+//**Formül: Kişinin kilosu / boy uzunluğunun karesi */
+// 3 - Hesaplanan indeks bilgisine göre karşılaştırma yapınız.
+// 4 - Aşağıdaki tabloya göre can ve ada hangi gruba giriyor.
+
+// 0 - 18,4: Zayıf
+// 18,5 - 24,9 : normal
+// 25,0 - 29,9 : fazla kilolu
+// 30,0 - 34,9 ; şişmaz(obez)
+
+const kgCan= 90;
+const boyCan=1.80;
+const kgAda= 40;
+const boyAda= 1.60;
+IndexCan =(kgCan) / (boyCan*boyCan);
+IndexAda = (kgAda) / (boyAda*boyAda);
+//.toFixed küsüratı yuvarlamak için 17.777 değilde direk 18 yazdırmak için kullanılır
+console.log(IndexAda.toFixed(),IndexCan.toFixed());
+
+let adaHigherIndex= IndexAda>IndexCan;
+let canHigherIndex= IndexCan>IndexAda;
+console.log("adanın indeksi candan daha büyüktürk ?"+adaHigherIndex);
+console.log('canın indenksi adadan daha büyüktür?'+canHigherIndex)
+
+let AdaZayif=(IndexAda>=0)&&(IndexAda<=18.4);
+let AdaNormal=(IndexAda>=18.5)&&(IndexAda<=25.9);
+let AdaFazlaKilolu=(IndexAda>=25.0)&&(IndexAda<=29.9);
+let AdaSisman=(IndexAda>=30)&&(IndexAda<=34.9);
+console.log("Ada zayıf"+AdaZayif);
+console.log("Ada normal"+AdaNormal);
+console.log("ada kilolu"+AdaFazlaKilolu);
+console.log("ada sisman"+AdaSisman);
+
+
+
+
