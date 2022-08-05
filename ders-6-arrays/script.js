e769a03ddaf4a358d40a8031862e67827477a81e
0xBerkecanAkdemir <berkeakdemirr@gmail.com>
2022-08-06 00:48:11 +0300

    arrays

---
+++ ders-6-arrays/script.js
@@ -0,0 +1,52 @@
+// ARRAYS
+// let names =['çınar','sena','ada','yiğit'];
+// let years=[1970,1999,2012,2010];
+// let mix=['sadık','turan',1983,null,undefined,['sinema','kitap']];
+
+// get array item 3.cü ismi gösterir
+// console.log(names[3])
+
+// set array item array içindeki ismi değiştirir
+// names[0]='emel';
+// names[names.length]='emel'; direk en sona emel diye ekler
+
+// add item
+
+// years.push(1986); en sona ekler
+// years.unshift(1986); en başa ekler
+
+// remove item
+
+// years.pop(); en sondaki elemaı siler
+// years.ship(); en baştaki elamanı siler
+
+// indexof dizinin içinde kaçıncı sırada olduğunu gösterir
+
+// let index = names.indexOf('ada');
+
+// reverse
+// names.reverse();
+
+// sort
+
+// years.sort(); küçükten büyüğe sıralar
+
+// concat
+// let val =years.concat(names);
+
+// splice birinci elemanı siler ve eleman ekler
+// names.splice(0,1);
+
+// find
+// let val =years.find(over18);
+
+// filter birden fazla elamanı yazdırabiliriz
+// function over18(year){
+//     let age = 2018-year;
+//     return age>=18;
+// }
+
+// let val =years.filter(over18);
+// console.log(val);
+
+// ARRAY UYGULAMALARI
