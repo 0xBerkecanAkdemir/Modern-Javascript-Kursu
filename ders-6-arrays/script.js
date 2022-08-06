c6c147fef9bdf6de4f1a86c7fe7619e70f4c4ca3
0xBerkecanAkdemir <berkeakdemirr@gmail.com>
2022-08-06 14:58:54 +0300

    arrays

--- ders-6-arrays/script.js
+++ ders-6-arrays/script.js
@@ -50,3 +50,55 @@
 // console.log(val);
 
 // ARRAY UYGULAMALARI
+
+// bmw,mercedes,opel,mazda elemanlarına sahip bir dizi oluşturun.
+
+let arr= ['bmw','mercedes','opel','mazda'];
+
+// dizi kaç elemanlı
+console.log(arr.length);
+// dizinin ilk ve son elemanı nedir ?
+console.log(arr[0]);
+console.log(arr[3]);
+
+// renault, fiat değerini dizinin sonuna ekleyin
+arr[4]='renault';
+arr[arr.length]='fiat';
+
+// dizinin sonuna toyota ekleyin
+arr.push('toyota');
+// dizinin başına seat ekleyin
+arr.unshift('seat');
+// seat değerini siliniz dizinin en başını shift ile sileriz
+arr.shift('seat');
+// toyota değerini siliniz dizinin son değerini siler
+arr.pop('toyota');
+// dizi elemanlarını ters çevirin
+arr.reverse();
+// dizi elemanlarını alfabetik sıraya göre sıralayın
+arr.sort();
+// [1,2,5,80] dizisini sıralayın.
+
+// Opel değeri dizinin bir elemanımıdır ?
+console.log(arr.indexOf('opel'));
+console.log(arr.includes('opel'));
+// var str = chevrolet,dacia ifadesini diziye çeviriniz.
+var str ='chevrolet, dacia';
+var arr2= str.split(',');
+console.log(arr2)
+
+// oluşturulan 2 dizinin elemanlarını bir başka dizi ile birleştirin.
+var arr3=arr.concat(arr2);
+console.log(arr3);
+// oluşturulan diziden son 2 elemanı siliniz. dizi 8 elemanlı son 2 6 dan başlayıp 2 karakter sil kodu:
+console.log(arr3.splice(6,2));
+
+// aşağıda verilen elemanları bir dizi içerisinde saklayınız.
+// aşağıdaki verilen elemanları biz dizi içerisinde saklayınız
+
+var studentA=['Yiğit','bilgi',2010];
+var studentB=['sena','turan',1999];
+var studentC=['ahmet','turan',1998];
+
+var students=[studentA,studentB,studentC];
+console.log(students);
